9f4fd69b2923545775af747e6535e1a42f45fd7a
Alkl58 <34812414+Alkl58@users.noreply.github.com>
2020-04-25 14:43:40 +0200

    Add files via upload

---
+++ AutoTagger.py
@@ -0,0 +1,106 @@
+import requests
+import json
+import bs4
+import re
+import time
+import argparse
+import lxml.html
+from requests.auth import HTTPBasicAuth
+import urllib
+
+#######################################################################################################################
+#       Use Embedded Login / Adress:                                                                                  #
+#                                                                                                                     #
+embeddedauth = False #set this to "True" to skip command args                                                         #
+#                                                                                                                     #
+#                                                                                                                     #
+booruadresse = "http://website:80/" #Insert here your Public Booru Adress - must be reachable from outside            #
+booruloginname = "username" #Booru Username - must have high enough privileges                                        #
+booruloinpassw = "password" # Booru User Password                                                                     #
+#                                                                                                                     #
+#######################################################################################################################
+
+postnummer = "68" #Automaticly set - don't touch
+postversion = "0" #Automaticly set - don't touch
+
+parser = argparse.ArgumentParser(description='Commandline usage of AutoTagger - You can skip username, password and adress, when you edited this Python script by setting "embeddedauth" to "True"')
+if embeddedauth == False:
+    parser.add_argument("--username", required=True, help="Username of the Account on the Booru")
+    parser.add_argument("--password", required=True, help="Password of the Account on the Booru")
+    parser.add_argument("--adress", required=True, help="The Adress of the Booru, must be reachable from outside of your network")
+parser.add_argument("--mode", type=int, help="1 = Single Post, 2 = Range of Posts - Default: 1")
+parser.add_argument("--poststart", required=True, help="Post (start) number - Must be lower than 'post-end' when 'mode = 2'")
+parser.add_argument("--postend", help="Post number - Only for 'mode = 2'")
+args = parser.parse_args()
+
+if embeddedauth == False:
+    booruloginname = args.username
+    booruloinpassw = args.password
+    booruadresse = args.adress
+    
+postnummerstart = str(args.poststart)
+postnummerende = str(args.postend)
+modus = args.mode
+
+if(modus == None):
+    modus = 1
+
+def RequestPostAdresse():
+    #Diese Funktion fragt die URL des Bildes ab
+    r2 = requests.get(booruadresse + "/api/post/" + postnummer, headers={'Accept':'application/json'}, auth=HTTPBasicAuth(booruloginname, booruloinpassw))
+    r3 = r2.json()["contentUrl"]
+    r4 = r2.json()["version"]
+    addresseTemp = booruadresse + r3
+    global postversion
+    postversion = r4
+    #print(postversion)
+    #print(addresseTemp)
+    IQDBAbfrage(addresseTemp)
+
+def IQDBAbfrage(url):
+    #Diese Funkion lädt das Bild auf IQDB hoch und zieht sich die Tags aus dem "Best-Match" Bild
+    res = requests.get('https://iqdb.org/?url=' + url)
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    #Filter um "nur" <img...> zu filtern
+    elems = soup.select('#pages > div:nth-child(2) > table:nth-child(1) > tr:nth-child(2) > td > a > img')
+    #Entfernt die ersten fünf Elemente, da diese irrelevant sind
+    #print(soup)
+    tags = elems[0].get('title').split()[5:]
+    print(tags)
+    UpdateTag(tags)
+
+def UpdateTag(tagsinput):
+    #Diese Funktion macht ein Put-Request, um die Tags auf dem Booru zu setzen
+    #So muss Tagging aussehen: < 'big_breasts' "," 'sex' >
+    taginput = {"version": postversion, "tags": tagsinput}
+    tagtemp = json.dumps(taginput)
+    r2 = requests.put(booruadresse + "/api/post/" + postnummer, headers={'Accept':'application/json', 'Content-Type': 'application/json'}, auth=HTTPBasicAuth(booruloginname, booruloinpassw), data=tagtemp)
+    #print(r2.json())
+    #print(taginput)
+
+def PostLoop(start, ende):
+    #Loop durch die Posts durch. Kann Problematisch sein, da manche Bilder einfach keine Tags zurückgeben und daher die Funktionen abschmieren
+    nekomimis = 0
+    catgirls = 1
+    nekomimis = int(start)
+    catgirls = int(ende)
+    global postnummer
+    while nekomimis <= catgirls:
+        postnummer = str(nekomimis)
+        try:
+            RequestPostAdresse()
+        except:
+            print("Error with Post " + str(nekomimis))
+        nekomimis += 1
+        time.sleep(10) #Waits 10s after each request, so IQDB won't block you
+    print("Finished!")
+
+
+if(modus == 1):
+    print("Modus 1")
+    postnummer = args.poststart
+    RequestPostAdresse()
+    print("Finished!")
+elif(modus == 2):
+    print("Modus 2")
+    PostLoop(postnummerstart, postnummerende)
